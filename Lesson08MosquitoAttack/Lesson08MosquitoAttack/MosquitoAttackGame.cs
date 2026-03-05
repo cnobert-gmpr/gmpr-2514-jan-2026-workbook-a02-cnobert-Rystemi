494071fdee2b4eaebb952a9e48f7f05838bfea15
Rystemi <lolgamer6205@gmail.com>
2026-03-05 11:30:21 -0700

    pause, move cannon

--- Lesson08MosquitoAttack/Lesson08MosquitoAttack/MosquitoAttackGame.cs
+++ Lesson08MosquitoAttack/Lesson08MosquitoAttack/MosquitoAttackGame.cs
@@ -14,6 +14,11 @@ public class MosquitoAttackGame : Game
 
     private Texture2D _background;
     private SpriteFont _font;
+    private string _message = "";
+    private KeyboardState _kbPrevState, _kbCurrentState;
+
+    private enum GameState { Playing, Paused, Over }
+    private GameState _gameState;
 
     Cannon _cannon;
 
@@ -31,7 +36,9 @@ public class MosquitoAttackGame : Game
         _graphics.ApplyChanges();
 
         _cannon = new Cannon();
-        _cannon.Initialize(new Vector2(50, 325));
+        _cannon.Initialize(new Vector2(50, 325), 150f);
+
+        _gameState = GameState.Playing;
 
         base.Initialize();
     }
@@ -49,22 +56,70 @@ public class MosquitoAttackGame : Game
 
     protected override void Update(GameTime gameTime)
     {
-        _cannon.Update(gameTime);
-        
+        _kbCurrentState = Keyboard.GetState();
+
+        switch(_gameState)
+        {
+            case GameState.Playing:
+                #region keyboard input
+                if(_kbCurrentState.IsKeyDown(Keys.Left) || _kbCurrentState.IsKeyDown(Keys.A)) 
+                    _cannon.Direction = new Vector2(-1, 0);
+                else if(_kbCurrentState.IsKeyDown(Keys.Right) || _kbCurrentState.IsKeyDown(Keys.D))
+                    _cannon.Direction = new Vector2(1, 0);
+                else
+                    _cannon.Direction = Vector2.Zero;
+
+                if (Pressed(Keys.P))
+                    {
+                        _gameState = GameState.Paused;
+                        _message = "Game Paused, Press P to continue.";
+                    }
+                #endregion
+                _cannon.Update(gameTime);
+                break;
+            case GameState.Paused:
+                if (Pressed(Keys.P))
+                    {
+                        _gameState = GameState.Playing;
+                        _message = "";
+                    }
+                break;
+            case GameState.Over:
+                break;
+            
+            
+        }
+        _kbPrevState = _kbCurrentState;
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
+        GraphicsDevice.Clear(Color.White);
 
         _spriteBatch.Begin();
-        _spriteBatch.Draw(_background, Vector2.Zero, Color.White);
 
-        _cannon.Draw(_spriteBatch);
+        switch(_gameState)
+        {
+            case GameState.Playing:
+                _spriteBatch.Draw(_background, Vector2.Zero, Color.White);
+                _cannon.Draw(_spriteBatch);
+                break;
+            case GameState.Paused:
+                _spriteBatch.Draw(_background, Vector2.Zero, Color.Silver);
+                _cannon.Draw(_spriteBatch);
+                _spriteBatch.DrawString(_font, _message, new Vector2 (125,175), Color.White);
+                break;
+            case GameState.Over:
+                break;
+        }
 
         _spriteBatch.End();
 
         base.Draw(gameTime);
+    }
+    private bool Pressed(Keys key)
+    {
+        return _kbCurrentState.IsKeyDown(Keys.P) && _kbPrevState.IsKeyUp(Keys.P);
     }
 }
